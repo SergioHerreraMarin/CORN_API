8814acc81874615c506fed6b61528e221271b5ff
SergioHerreraMarin <sergioherreramarin@gmail.com>
2023-02-15 19:42:05 +0100

    update app.js code

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const fs = require('fs/promises')
-const url = require('url')
-const post = require('./post.js')
-const { v4: uuidv4 } = require('uuid')
-const mysql = require('mysql2');
+const express     = require('express')
+const fs          = require('fs').promises
 
-// Wait 'ms' milliseconds
-function wait (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+const webSockets  = require('./appWS.js')
+const post        = require('./utilsPost.js')
+const database    = require('./utilsMySQL.js')
+const wait        = require('./utilsWait.js')
+
+var db = new database()   // Database example: await db.query("SELECT * FROM test")
+var ws = new webSockets()
 
 // Start HTTP server
 const app = express()
-
-// Set port number
 const port = process.env.PORT || 3000
 
 // Publish static files from 'public' folder
@@ -25,43 +22,83 @@ function appListen () {
   console.log(`Listening for HTTP queries on: http://localhost:${port}`)
 }
 
-// Set URL rout for POST queries
-app.post('/dades', getDades)
-async function getDades (req, res) {
+// Close connections when process is killed
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+function shutDown() {
+  console.log('Received kill signal, shutting down gracefully');
+  httpServer.close()
+  db.end()
+  ws.end()
+  process.exit(0);
+}
+
+// Init objects
+db.init({
+  host: process.env.MYSQLHOST || "localhost",
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "",
+  database: process.env.MYSQLDATABASE || "Corndb"
+})
+ws.init(httpServer, port, db)
+
+
+// Define routes
+app.post('/dades', getPostDades)
+async function getPostDades (req, res) {
+
   let receivedPOST = await post.getPostObject(req)
   let result = { status: "KO", result: "Unkown type" }
 
   var textFile = await fs.readFile("./public/consoles/consoles-list.json", { encoding: 'utf8'})
   var objConsolesList = JSON.parse(textFile)
 
-  switch(receivedPOST.type){
+  if (receivedPOST) {
 
-    case "consola":
-      var objFilteredList = objConsolesList.filter((obj) => { return obj.name == receivedPOST.name })
-      await wait(1500)
-      if (objFilteredList.length > 0) {
-        result = { status: "OK", result: objFilteredList[0] }
-      }
-    break;
-    
-    case "marques":
-      var objBrandsList = objConsolesList.map((obj) => { return obj.brand })
-      await wait(1500)
-      let senseDuplicats = [...new Set(objBrandsList)]
-      result = { status: "OK", result: senseDuplicats.sort() } 
-    break;
+    switch(receivedPOST.type){
 
-    case "marca":
-      var objBrandConsolesList = objConsolesList.filter ((obj) => { return obj.brand == receivedPOST.name })
-      await wait(1500)
-      // Ordena les consoles per nom de model
-      objBrandConsolesList.sort((a,b) => { 
-          var textA = a.name.toUpperCase();
-          var textB = b.name.toUpperCase();
-          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-      })
-      result = { status: "OK", result: objBrandConsolesList } 
-    break;
+      case "consola":
+        var objFilteredList = objConsolesList.filter((obj) => { return obj.name == receivedPOST.name })
+        await wait(1500)
+        if (objFilteredList.length > 0) {
+            result = { status: "OK", result: objFilteredList[0] }
+        }
+      break;
+
+      case "marques":
+        var objBrandsList = objConsolesList.map((obj) => { return obj.brand })
+        await wait(1500)
+        let senseDuplicats = [...new Set(objBrandsList)]
+        result = { status: "OK", result: senseDuplicats.sort() }
+      break;
+
+      case "marca":
+        var objBrandConsolesList = objConsolesList.filter ((obj) => { return obj.brand == receivedPOST.name })
+        await wait(1500)
+        objBrandConsolesList.sort((a,b) => { 
+            var textA = a.name.toUpperCase();
+            var textB = b.name.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        })
+        result = { status: "OK", result: objBrandConsolesList }
+      break;
+
+      case "addNewUser":
+        //Mirar si existe
+
+        //Fatlta terminar xd 
+        try{
+          const result = await db.query("insert into Users(userName, userLastName, userEmail, userPhoneNumber, balance) values('"+ receivedPOST.name +"', '"+ receivedPOST.lastName +"', '"+ receivedPOST.email +"', '"+ receivedPOST.phoneNumber +"', '"+ receivedPOST.balance +"');")
+          result = { status: "OK", result: "result" }
+        }catch(error){
+          result = { status: "OK", result: "noResult"}
+        }
+        
+      break;
+
+    }
+
   }
 
   res.writeHead(200, { 'Content-Type': 'application/json' })
@@ -69,107 +106,3 @@ async function getDades (req, res) {
 }
 
 
-
-
-// Run WebSocket server
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ server: httpServer })
-const socketsClients = new Map()
-console.log(`Listening for WebSocket queries on ${port}`)
-
-// What to do when a websocket client connects
-wss.on('connection', (ws) => {
-
-  console.log("Client connected")
-
-  // Add client to the clients list
-  const id = uuidv4()
-  const color = Math.floor(Math.random() * 360)
-  const metadata = { id, color }
-  socketsClients.set(ws, metadata)
-
-  // Send clients list to everyone
-  sendClients()
-
-  // What to do when a client is disconnected
-  ws.on("close", () => {
-    socketsClients.delete(ws)
-  })
-
-  // What to do when a client message is received
-  ws.on('message', (bufferedMessage) => {
-    var messageAsString = bufferedMessage.toString()
-    var messageAsObject = {}
-    
-    try { messageAsObject = JSON.parse(messageAsString) } 
-    catch (e) { console.log("Could not parse bufferedMessage from WS message") }
-
-    if (messageAsObject.type == "bounce") {
-      var rst = { type: "bounce", message: messageAsObject.message }
-      ws.send(JSON.stringify(rst))
-    } else if (messageAsObject.type == "broadcast") {
-      var rst = { type: "broadcast", origin: id, message: messageAsObject.message }
-      broadcast(rst)
-    } else if (messageAsObject.type == "private") {
-      var rst = { type: "private", origin: id, destination: messageAsObject.destination, message: messageAsObject.message }
-      private(rst)
-    }
-  })
-})
-
-// Get clients ids
-function sendClients () {
-  var clients = []
-  socketsClients.forEach((value, key) => {
-    clients.push(value.id)
-  })
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      var id = socketsClients.get(client).id
-      var messageAsString = JSON.stringify({ type: "clients", id: id, list: clients })
-      client.send(messageAsString)
-    }
-  })
-}
-
-// Send a message to all websocket clients
-async function broadcast (obj) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      var messageAsString = JSON.stringify(obj)
-      client.send(messageAsString)
-    }
-  })
-}
-
-// Send a private message to a specific client
-async function private (obj) {
-  wss.clients.forEach((client) => {
-    if (socketsClients.get(client).id == obj.destination && client.readyState === WebSocket.OPEN) {
-      var messageAsString = JSON.stringify(obj)
-      client.send(messageAsString)
-      return
-    }
-  })
-}
-
-// Perform a query to the database
-function queryDatabase (query) {
-
-  return new Promise((resolve, reject) => {
-    var connection = mysql.createConnection({
-      host: process.env.MYSQLHOST || "localhost",
-      port: process.env.MYSQLPORT || 3306,
-      user: process.env.MYSQLUSER || "root",
-      password: process.env.MYSQLPASSWORD || "",
-      database: process.env.MYSQLDATABASE || "test"
-    });
-
-    connection.query(query, (error, results) => { 
-      if (error) reject(error);
-      resolve(results)
-    });
-     
-    connection.end();
-  })
-}
