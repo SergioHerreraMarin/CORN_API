527e35920ee84901ebabf525fae91b858523e3f6
BorjaMontoro <bmontoroplaza.cf@iesesteveterradas.cat>
2023-02-16 17:28:29 +0100

    Funciones API

--- app.js
+++ app.js
@@ -45,61 +45,114 @@ ws.init(httpServer, port, db)
 
 
 // Define routes
-app.post('/dades', getPostDades)
-async function getPostDades (req, res) {
+app.post('/api/signup', signup)
+async function signup (req, res) {
+
+  let receivedPOST = await post.getPostObject(req)
+  let result = { status: "KO", message: "Unkown type" }
+
+  if (receivedPOST) {
+    var regex = /^(\d{9})$/;
+    if (regex.test(receivedPOST.phoneNumber)){
+      const existe = await db.query("select count(*) from Users where userPhoneNumber="+receivedPOST.phoneNumber);
+      if (Object.values(existe[0])==0){
+        regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/;
+        if (regex.test(receivedPOST.name)){
+          regex = /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/;
+          if (regex.test(receivedPOST.lastName) && receivedPOST.lastName.trim()!=""){
+            regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+            if (regex.test(receivedPOST.email)){
+              await db.query("insert into Users(userName, userLastName, userEmail, userPhoneNumber, balance) values('"+ receivedPOST.name +"', '"+ receivedPOST.lastName +"', '"+ receivedPOST.email +"', "+ receivedPOST.phoneNumber +", '"+ receivedPOST.balance +"');")
+              result = { status: "OK", message: "Insert" }
+            }
+            else{
+              result = {status: "ERROR", message: "Email is not valid"}
+            }
+          }else{
+            result = {status: "ERROR", message: "Surname is not valid"}
+          }
+        }else{
+          result = {status: "ERROR", message: "Name is not valid"}
+        }
+      } else{
+        result = {status: "OK", message: "Exist"};
+      }
+    }else{
+      result = {status: "ERROR", message: "Phone is not valid"}
+    }
+        
+    }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result))
+}
+
+// Define routes
+app.post('/api/get_profiles', getProfiles)
+async function getProfiles (req, res) {
 
   let receivedPOST = await post.getPostObject(req)
   let result = { status: "KO", result: "Unkown type" }
 
-  var textFile = await fs.readFile("./public/consoles/consoles-list.json", { encoding: 'utf8'})
-  var objConsolesList = JSON.parse(textFile)
-
   if (receivedPOST) {
-
-    switch(receivedPOST.type){
-
-      case "consola":
-        var objFilteredList = objConsolesList.filter((obj) => { return obj.name == receivedPOST.name })
-        await wait(1500)
-        if (objFilteredList.length > 0) {
-            result = { status: "OK", result: objFilteredList[0] }
-        }
-      break;
-
-      case "marques":
-        var objBrandsList = objConsolesList.map((obj) => { return obj.brand })
-        await wait(1500)
-        let senseDuplicats = [...new Set(objBrandsList)]
-        result = { status: "OK", result: senseDuplicats.sort() }
-      break;
-
-      case "marca":
-        var objBrandConsolesList = objConsolesList.filter ((obj) => { return obj.brand == receivedPOST.name })
-        await wait(1500)
-        objBrandConsolesList.sort((a,b) => { 
-            var textA = a.name.toUpperCase();
-            var textB = b.name.toUpperCase();
-            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-        })
-        result = { status: "OK", result: objBrandConsolesList }
-      break;
-
-      case "addNewUser":
-        //Mirar si existe
-
-        //Fatlta terminar xd 
-        try{
-          const result = await db.query("insert into Users(userName, userLastName, userEmail, userPhoneNumber, balance) values('"+ receivedPOST.name +"', '"+ receivedPOST.lastName +"', '"+ receivedPOST.email +"', '"+ receivedPOST.phoneNumber +"', '"+ receivedPOST.balance +"');")
-          result = { status: "OK", result: "result" }
-        }catch(error){
-          result = { status: "OK", result: "noResult"}
-        }
-        
-      break;
-
+    const usuarios = await db.query("select * from Users");
+    result= {status: "OK", profiles: usuarios}
     }
 
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result))
+}
+
+// Define routes
+app.post('/api/setup_payment', setupPayment)
+async function setupPayment (req, res) {
+
+  let receivedPOST = await post.getPostObject(req)
+  let result = { status: "KO", result: "Unkown type" }
+  let comprobacion=false;
+  if (receivedPOST) {
+    if(receivedPOST.phoneNumber==""){
+      result = {status: "ERROR", message: "user_id is required"}
+    } else{
+      var regex = /^(\d{9})$/;
+      if (regex.test(receivedPOST.phoneNumber)){
+        const existe = await db.query("select count(*) from Users where userPhoneNumber="+receivedPOST.phoneNumber);
+        if (Object.values(existe[0])>=0){
+          var regex = /^[0-9]+$/;
+            if (receivedPOST.amount.indexOf(".") === -1 ){
+                if (regex.test(receivedPOST.amount)){
+                   comprobacion=true;
+                }else{
+                  comprobacion=false;
+                }
+            }else{
+                var particion = receivedPOST.amount.split(".");
+                if (regex.test(particion[0])){
+                    if (regex.match(particion[1])){
+                      comprobacion=true;
+                    }else {
+                      comprobacion=false;
+                    }
+                }else{
+                  comprobacion=false;
+                }
+            }
+            if (comprobacion===true){
+              //const 
+              //await db.query("insert into Transactions(destination,token,timeCreate) values ('"+receivedPOST.+"','"++"','"++"')");
+            } else{
+              result = {status: "ERROR", message: "Wrong amount"};
+            }
+        } else{
+          result = {status: "ERROR", message: "No Exist"};
+        }
+      }else{
+        result = {status: "ERROR", message: "Phone is not valid"}
+      }
+      //const usuarios = await db.query("select * from Users");
+      //result= {status: "OK", profiles: usuarios}  
   }
+}
 
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.end(JSON.stringify(result))
