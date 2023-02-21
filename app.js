5d1b5df68e373cff6e10f4af6b897dfc66d2ea68
SergioHerreraMarin <sergioherreramarin@gmail.com>
2023-02-21 16:19:51 +0100

    setup_payment done

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ db.init({
   host: process.env.MYSQLHOST || "localhost",
   port: process.env.MYSQLPORT || 3306,
   user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "",
+  password: process.env.MYSQLPASSWORD || "p@ssw0rd",
   database: process.env.MYSQLDATABASE || "Corndb"
 })
 ws.init(httpServer, port, db)
@@ -111,15 +111,15 @@ async function setupPayment (req, res) {
   let result = { status: "KO", result: "Unkown type" }
   let comprobacion=false;
   if (receivedPOST) {
-    if(receivedPOST.phoneNumber==""){
+    if(receivedPOST.user_id==""){
       result = {status: "ERROR", message: "user_id is required"}
     } else{
       var regex = /^(\d{9})$/;
-      if (regex.test(receivedPOST.phoneNumber)){
-        const existe = await db.query("select count(*) from Users where userPhoneNumber="+receivedPOST.phoneNumber);
-        if (Object.values(existe[0])>=0){
+      if (regex.test(receivedPOST.user_id)){
+        const existe = await db.query("select count(*) from Users where userPhoneNumber="+receivedPOST.user_id);
+        if (Object.values(existe[0])>0){
           var regex = /^[0-9]+$/;
-            if (receivedPOST.amount.indexOf(".") === -1 ){
+            if (String(receivedPOST.amount).indexOf('.') === -1){
                 if (regex.test(receivedPOST.amount)){
                    comprobacion=true;
                 }else{
@@ -137,25 +137,53 @@ async function setupPayment (req, res) {
                   comprobacion=false;
                 }
             }
-            if (comprobacion===true){
-              //const 
-              //await db.query("insert into Transactions(destination,token,timeCreate) values ('"+receivedPOST.+"','"++"','"++"')");
+            if(comprobacion===true){
+              let token = createToken();
+              let currentDate = new Date();
+              let timeSetup = String(currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate() + " " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds());
+
+              await db.query("insert into Transactions(destination,amount,token,accepted,timeSetup) values ('"+receivedPOST.user_id+"','"+receivedPOST.amount+"','"+token+"',FALSE,'"+ timeSetup +"');");
+              result = {status: "OK", transaction_token: token};
             } else{
-              result = {status: "ERROR", message: "Wrong amount"};
+              result = {status: "ERROR", message:"ERROR"};
             }
         } else{
           result = {status: "ERROR", message: "No Exist"};
         }
       }else{
-        result = {status: "ERROR", message: "Phone is not valid"}
-      }
-      //const usuarios = await db.query("select * from Users");
-      //result= {status: "OK", profiles: usuarios}  
+        result = {status: "ERROR", message: "ID is not valid"}
+      }  
   }
 }
-
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.end(JSON.stringify(result))
 }
+
+
+function createToken(){
+  let charsList = [];
+  let tokenSize = 25;
+  let token = "P";
+
+  for(i = 0; i < 10; i ++){
+    charsList.push(i);
+  }
+
+  for(i = 65; i <= 90; i++) {
+    charsList.push(String.fromCharCode(i));
+  }
+
+  for(i = 97; i <= 122; i++) {
+    charsList.push(String.fromCharCode(i));
+  }
+  
+  for(i = 0; i < tokenSize - 1; i++){
+    let randomNum = Math.round(Math.random()*(charsList.length - 1));
+    token += charsList[randomNum];
+  }
+
+  return token;
+}
+
 
 
